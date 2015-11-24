fa8a40614e2a4055d5e0785e396caac7470628be
deepu12 <saimanideep.t@gmail.com>
2015-11-24 14:53:48 +0530

    Displaying User Docs

--- client/Docs/docDisplay.js
+++ client/Docs/docDisplay.js
@@ -1,13 +1,20 @@
 Template.docDisplay.helpers({
-   fileId : function(){
+   fileID : function(){
       var dId = Docs.find({userId : Meteor.userId()}).fetch();
       console.log("This is the file Id : ",dId.fileId);
-      var fid;
-      while(dId.length > 0){
-         console.log("This is the file Id to find FID ",dId.fileId);
-          fid = Files.findOne({ _id : dId.fileId});
+      var mapFun = _.map(dId,function(file){
+         console.log("In Map function :",file.fileId);
+         var fid = Files.findOne(file.fileId);
+         console.log("got FID :",fid);
          return fid;
-      }
+      })
+      return mapFun;
+
+      // while(dId.length > 0){
+      //    console.log("This is the file Id to find FID ",dId.fileId);
+      //     fid = Files.findOne({ _id : dId.fileId});
+      //    return fid;
+      // }
    }
    // docId : function(){
    //    var nId = Session.get('newId');
