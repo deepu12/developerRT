85a4da2ed514a3b69ac1db32197a59494af3bd56
deepu12 <saimanideep.t@gmail.com>
2015-11-24 12:40:43 +0530

    Search box for projects

--- client/Docs/docDisplay.js
+++ client/Docs/docDisplay.js
@@ -1,13 +1,17 @@
 Template.docDisplay.helpers({
    fileId : function(){
-      var dId = Docs.find({userId : Meteor.userId()});
-      console.log("This is the Image Id : ",dId.fileId);
-      Session.set('newId',dId.fileId);
-      return dId;
-   },
-   docId : function(){
-      var nId = Session.get('newId');
-      var fid = Files.findOne(nId);
-      return fid;
+      var dId = Docs.find({userId : Meteor.userId()}).fetch();
+      console.log("This is the file Id : ",dId.fileId);
+      var fid;
+      while(dId.length > 0){
+         console.log("This is the file Id to find FID ",dId.fileId);
+          fid = Files.findOne({ _id : dId.fileId});
+         return fid;
+      }
    }
+   // docId : function(){
+   //    var nId = Session.get('newId');
+   //    var fid = Files.findOne({ _id : '7HJbpopPdCd4WBTDG'});
+   //    return fid;
+   // }
 })
---
+++ client/searchBox.js
@@ -0,0 +1,3 @@
+// Template.searchBox.helpers({
+//    projectsIndex: () => ProjectsIndex
+// })
