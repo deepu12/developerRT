59fc51ade91c58132b9c84ee4c5c3f61f3f3bed5
deepu12 <saimanideep.t@gmail.com>
2015-11-25 13:23:46 +0530

    Delete option for projects, Filtering projects by workspace

--- client/Projects/editProject.js
+++ client/Projects/editProject.js
@@ -4,4 +4,19 @@ Template.editProject.helpers({
       var ep = Project.findOne(this.projectId);
       return ep;
    }
-})
+});
+
+Template.editProject.events({
+   'click #btn-del' : function(event){
+      console.log("The project id in events : ",this.projectId);
+      var btnRemove = Project.remove(this.projectId);
+      if(btnRemove){
+         window.alert("Delete Successful");
+         FlowRouter.go('/newProject');
+      }
+      else {
+         window.alert("Delete failed");
+      }
+      return btnRemove;
+   }
+});
---
+++ client/Workspace/workspaceProject.js
@@ -0,0 +1,7 @@
+Template.workspaceProject.helpers({
+   wsProjects : function(){
+      console.log("Work space project Id :",this.workspaceId);
+      wsp = Project.find({workspaceId : this.workspaceId});
+      return wsp;
+   }
+})
